5533e130f9786d19d87dbb6b069b9134ced4b285
RodgeND24 <dima.chelokov00@gmail.com>
2025-08-12 16:12:58 +0300

    add CRUD operations for sql

--- server/app/api/services.py
+++ server/app/api/services.py
@@ -14,3 +14,19 @@ def get_users(db: Session):
 
 def get_user(db: Session, user_id: int):
     return db.query(Users).filter(Users.id == user_id).first()
+
+def update_user(db: Session, user: UserCreate, user_id: int):
+    user_query = db.query(Users).filter(Users.id == user_id).first()
+    if user_query:
+        for key, value in user.model_dump().items():
+            setattr(user_query, key, value)
+        db.commit()
+        db.refresh(user_query)
+    return user_query
+
+def delete_user(db: Session, user_id: int):
+    user = db.query(Users).filter(Users.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+    return user    
--- server/app/main.py
+++ server/app/main.py
@@ -7,22 +7,60 @@ from uvicorn import run
 
 app = FastAPI()
 
-@app.get("/users", tags=['Users'], response_model=list[schemas.schemas.User])
+@app.get(
+        "/users", 
+         tags=['Users'], 
+         summary = "Get all users", 
+         response_model=list[schemas.schemas.User]
+         )
 def get_all_users(db: Session = Depends(get_db)):
     return api.services.get_users(db)
 
-@app.get("/users/{user_id}", tags=['Users'], response_model=schemas.schemas.User | Dict[str,str])
+@app.get(
+        "/users/{user_id}",
+         tags = ['Users'],
+         summary = "Get user by user_id",
+         response_model = schemas.schemas.User | Dict[str,str]
+         )
 def get_all_users(user_id: int, db: Session = Depends(get_db)):
     user = api.services.get_user(db, user_id)
     if user:
         return user
     raise HTTPException(status_code=404, detail="User doesn't exist!")
 
-@app.post("/users", tags=['Users'], response_model=schemas.schemas.User)
+@app.post(
+        "/users", 
+        tags=['Users'], 
+        summary="Create new user",
+        response_model=schemas.schemas.User
+        )
 def create_new_user(user: schemas.schemas.UserCreate, db: Session = Depends(get_db)):
     return api.services.create_user(db, user)
 
-# @app.delete("/users/{user_id}")
+@app.put(
+        "/users/{user_id}", 
+        tags=['Users'], 
+        summary="Update user by user_id",
+        response_model=schemas.schemas.User
+        )
+def update_user(user: schemas.schemas.UserCreate, user_id: int, db: Session = Depends(get_db)):
+    db_update = api.services.update_user(db, user, user_id)
+    if not db_update:
+        raise HTTPException(status_code=404, detail="User not found!")
+    return db_update
+        
+
+@app.delete(
+        "/users/{user_id}", 
+        tags=['Users'], 
+        summary="Delete user by user_id",
+        response_model=schemas.schemas.User
+        )
+def delete_old_user(user_id: int, db: Session = Depends(get_db)):
+    user_delete = api.services.delete_user(db, user_id)
+    if not user_delete:
+        raise HTTPException(status_code=404, detail="User not found!")
+    return user_delete
 
 if __name__=="__main__":
     run("main:app", reload=True)
