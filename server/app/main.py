f69e33be456c86172cbd638a6fc9a0c84c1467d0
RodgeND24 <dima.chelokov00@gmail.com>
2025-08-19 17:21:32 +0300

    add initialization database after start server

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
 from api.router import router
+from db.database import init_db
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.on_event("startup")
+async def on_startup():
+    await init_db()
+
 app.include_router(router)
 
 
