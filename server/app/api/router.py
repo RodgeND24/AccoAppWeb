368984e88cd00ffa2b7db9a9cfd408b9fbf13c1c
RodgeND24 <dima.chelokov00@gmail.com>
2025-08-19 17:22:12 +0300

    add new routers for create/get users, create/update settings

--- server/app/api/router.py
+++ server/app/api/router.py
@@ -1,61 +1,77 @@
-import api.services, models.models, schemas.schemas
-from sqlalchemy.orm import Session
+import models.models as models, schemas.schemas as schemas
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends, HTTPException
-from db.db import engine, get_db
+from db.database import engine, get_db
+import db.crud as crud
 
 router = APIRouter(prefix="/users")
 
+'''Users'''
 @router.get(
         "", 
          tags=['Users'], 
          summary = "Get all users", 
-         response_model=list[schemas.schemas.User]
+         response_model=list[schemas.User]
          )
-def get_all_users(db: Session = Depends(get_db)):
-    return api.services.get_users(db)
+async def get_all_users(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)):
+    users = await crud.get_users(db=db, skip = skip, limit = limit)
+    return users
 
 @router.get(
-        "/{user_id}",
-         tags = ['Users'],
-         summary = "Get user by user_id",
-         response_model = schemas.schemas.User | dict[str,str]
+        "/username/{username}", 
+         tags=['Users'], 
+         summary = "Get users by username", 
+         response_model=schemas.User
          )
-def get_all_users(user_id: int, db: Session = Depends(get_db)):
-    user = api.services.get_user(db, user_id)
-    if user:
-        return user
-    raise HTTPException(status_code=404, detail="User doesn't exist!")
+async def get_user(username: str, db: AsyncSession = Depends(get_db)):
+    db_user = await crud.get_user_by_username(db=db, username = username)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@router.get(
+        "/id/{user_id}", 
+         tags=['Users'], 
+         summary = "Get users by id", 
+         response_model=schemas.User
+         )
+async def get_user(user_id: int, db: AsyncSession = Depends(get_db)):
+    db_user = await crud.get_user(db=db, user_id = user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
 @router.post(
         "", 
-        tags=['Users'], 
-        summary="Create new user",
-        response_model=schemas.schemas.User
-        )
-def create_new_user(user: schemas.schemas.UserCreate, db: Session = Depends(get_db)):
-    return api.services.create_user(db, user)
+         tags=['Users'], 
+         summary = "Create user", 
+         response_model=schemas.User
+         )
+async def create_user(user: schemas.UserCreate, db: AsyncSession = Depends(get_db)):
+    db_user = await crud.get_user_by_email(db=db, email = user.email)
+    if db_user:
+       raise HTTPException(status_code=400, detail="User already exist")
+    return await crud.create_user(db=db, user = user)
 
-@router.put(
-        "/{user_id}", 
-        tags=['Users'], 
-        summary="Update user by user_id",
-        response_model=schemas.schemas.User
-        )
-def update_user(user: schemas.schemas.UserCreate, user_id: int, db: Session = Depends(get_db)):
-    db_update = api.services.update_user(db, user, user_id)
-    if not db_update:
-        raise HTTPException(status_code=404, detail="User not found!")
-    return db_update
-        
 
-@router.delete(
-        "/{user_id}", 
-        tags=['Users'], 
-        summary="Delete user by user_id",
-        response_model=schemas.schemas.User
-        )
-def delete_old_user(user_id: int, db: Session = Depends(get_db)):
-    user_delete = api.services.delete_user(db, user_id)
-    if not user_delete:
-        raise HTTPException(status_code=404, detail="User not found!")
-    return user_delete
+'''Settings'''
+@router.get(
+        "/{user_id}/settings", 
+         tags=['Settings'], 
+         summary = "Get user's settings", 
+         response_model=schemas.Settings
+         )
+async def get_settings(user_id: int, db: AsyncSession = Depends(get_db)):
+    db_user_settings = await crud.get_settings(db=db, user_id = user_id)
+    if not db_user_settings:
+        raise HTTPException(status_code=404, detail="User's settings not found")
+    return db_user_settings
+
+@router.post(
+        "/{user_id}/settings", 
+         tags=['Settings'], 
+         summary = "Set all user's settings", 
+         response_model=schemas.SettingsCreate
+         )
+async def create_settings(user_id: int, settings: schemas.SettingsCreate, db: AsyncSession = Depends(get_db)):
+   return await crud.create_settings(db=db, settings=settings, user_id=user_id)
