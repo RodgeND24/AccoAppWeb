cec3c7b544ac983d239d4ced74e7344b859ed4d2
RodgeND24 <dima.chelokov00@gmail.com>
2025-08-19 17:25:27 +0300

    add pydantic-schemes for validation

--- server/app/schemas/schemas.py
+++ server/app/schemas/schemas.py
@@ -1,19 +1,82 @@
-from pydantic import BaseModel, Field
+# Pydantic-schemas for database tables
 
-class UsersBase(BaseModel):
-    username: str
+from pydantic import BaseModel, EmailStr, Field, field_validator
+from typing import Optional, List
+from datetime import datetime, date
+
+'''Classes for Settings'''
+class SettingsBase(BaseModel):
     firstname: str
-    lastname: str
-    age: int
-    email: str
-    phone: str 
-    about: str = None
+    lastname: str | None = None
+    description: str | None = None
+    work: str | None = None
+    language: str
+    country: str | None = None
+    theme: str
 
-class UserCreate(UsersBase):
+class SettingsCreate(SettingsBase):
     pass
 
-class User(UsersBase):
+class Settings(SettingsBase):
     id: int
+    user_id: int
+    is_online: bool = False
 
-    class config:
-        from_attribute = True
+    class Config:
+        from_attributes = True
+
+'''Classes for Profits'''
+class ProfitsBase(BaseModel):
+    type: str
+    sum: int
+    date: date
+
+class ProfitsCreate(ProfitsBase):
+    pass
+
+class Profits(ProfitsBase):
+    id: int
+    user_id: int
+
+    class Config:
+        from_attributes = True
+
+'''Classes for Expenses'''
+class ExpensesBase(BaseModel):
+    shop: str
+    sum: int
+    date: date
+
+class ExpensesCreate(ExpensesBase):
+    pass
+
+class Expenses(ExpensesBase):
+    id: int
+    user_id: int
+
+    class Config:
+        from_attributes = True
+
+'''Classes for Users'''
+class UserBase(BaseModel):
+    username: str = Field(min_length=3, max_length=20)
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str = Field(min_length=10)
+
+
+    @field_validator("password")
+    def check_password(cls, value):
+        not_access_symbols = '\'\"[]{};:=><|()~`\\'
+        for symbol in not_access_symbols:
+            if symbol in value:
+                raise ValueError("Password contains invalid symbols")
+        return value
+
+class User(UserBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
