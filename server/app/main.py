cbb63a4a8d9bdd70ebd1436418007b239327bf64
RodgeND24 <dima.chelokov00@gmail.com>
2025-08-29 20:20:42 +0300

    add origins to nginx, frontend (future);
    Need add lifespan instead .on_event

--- server/app/main.py
+++ server/app/main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
-from api.router import router
+from api.router import router as router_crud
+from core.auth import router as router_auth
 from db.database import init_db
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:80",
+    "http://localhost:8080",
+    "http://localhost:3000"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -18,7 +26,8 @@ app.add_middleware(
 async def on_startup():
     await init_db()
 
-app.include_router(router)
+app.include_router(router_crud)
+app.include_router(router_auth)
 
 
 if __name__=="__main__":
